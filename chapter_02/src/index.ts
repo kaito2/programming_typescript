07850821677776c9a1da04ff05190e6697327cb3
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-06-15 01:24:55 +0900

    chapter2 ex

--- chapter_02/src/index.ts
+++ chapter_02/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello TypeScript!');
+let a = 1 + 2
+let b = a + 3
+let c = {
+    apple: a,
+    banana: b
+}
+// error
+// a = "hello"
+// c.apple = 'a'
+// let e = c.apple * "10"
+let d = c.apple * 4
